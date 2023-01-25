8f77c347cbf23db8fe7609d29234f5cd3d784a47
bruno paixao <brunoamorimpaixao@gmail.com>
2023-01-24 23:37:47 -0300

    criando o projeto cliente

---
+++ cliente/src/main/java/com/cliente/model/Cliente.java
@@ -0,0 +1,15 @@
+package com.cliente.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+
+    private int id;
+    private String nome;
+
+}
