ae22f7c0e52f50456f19cd55946441c5fae1f397
bruno paixao <brunoamorimpaixao@gmail.com>
2023-01-24 23:39:15 -0300

    criando o DTO cliente

---
+++ cliente/src/main/java/com/cliente/dto/ClienteDTO.java
@@ -0,0 +1,22 @@
+package com.cliente.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClienteDTO {
+
+    @NotNull(message = "Id é obrigatório.")
+    private int id;
+    @NotEmpty(message = "Nome é obrigatório.")
+    @Size(max = 100, message = "Tamanho máximo permitido de 100 caracteres.")
+    private String nome;
+}
