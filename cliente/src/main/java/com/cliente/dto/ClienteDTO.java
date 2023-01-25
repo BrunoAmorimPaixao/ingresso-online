86d47cef2ba45550b16c9940a0277b343b7dd498
bruno paixao <brunoamorimpaixao@gmail.com>
2023-01-25 00:27:56 -0300

    ajuste do campos cpf no clente

--- cliente/src/main/java/com/cliente/dto/ClienteDTO.java
+++ cliente/src/main/java/com/cliente/dto/ClienteDTO.java
@@ -1,12 +1,12 @@
 package com.cliente.dto;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
 @Data
 @Builder
@@ -14,9 +14,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ClienteDTO {
 
-    @NotNull(message = "Id é obrigatório.")
-    private Long id;
     @NotEmpty(message = "Nome é obrigatório.")
     @Size(max = 100, message = "Tamanho máximo permitido de 100 caracteres.")
     private String nome;
+    @NotEmpty(message = "CPF é obrigatório.")
+    @CPF(message = "CPF inválido")
+    @Size(max = 14, message = "Tamanho máximo permitido de 14 caracteres.")
+    private String cpf;
 }
--- cliente/src/main/java/com/cliente/model/Cliente.java
+++ cliente/src/main/java/com/cliente/model/Cliente.java
@@ -1,5 +1,9 @@
 package com.cliente.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +13,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pessoa")
     private Long id;
+
+    @Column(name = "nome", nullable = false, length = 100)
     private String nome;
+
+    @Column(name = "cpf", nullable = false,  length = 14, unique = true)
+    private String cpf;
 
 }
