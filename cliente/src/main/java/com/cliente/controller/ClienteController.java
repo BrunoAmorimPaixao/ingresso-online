a2a9f3e53355ca123a02e782fd72c30539e9f590
bruno paixao <brunoamorimpaixao@gmail.com>
2023-01-24 23:40:11 -0300

    criando o controller cliente

---
+++ cliente/src/main/java/com/cliente/controller/ClienteController.java
@@ -0,0 +1,33 @@
+package com.cliente.controller;
+
+import com.cliente.dto.ClienteDTO;
+import com.cliente.model.Cliente;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+@Slf4j
+public class ClienteController {
+
+    private final ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ClienteDTO create(@RequestBody @Valid ClienteDTO dto) {
+        log.info(" criando um  agência com número: {} ", dto.getNome());
+        Cliente entity = modelMapper.map(dto,  Cliente.class);
+        //entity =  service.save(entity, dto.getIdBanco());
+        return modelMapper.map(entity,  ClienteDTO.class);
+    }
+
+}
