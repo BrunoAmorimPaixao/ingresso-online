1a060101a6aa9a4e4735d5553ba63ec5859a1303
bruno paixao <brunoamorimpaixao@gmail.com>
2023-03-20 00:09:53 -0300

    add o bean modelmap

--- evento/src/main/java/com/evento/EventoApplication.java
+++ evento/src/main/java/com/evento/EventoApplication.java
@@ -1,13 +1,20 @@
 package com.evento;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EventoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventoApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }
